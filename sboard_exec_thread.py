58124346a757fb7fb600e3d24cecca55413aaaa0
pgv123 <32214613+pgv123@users.noreply.github.com>
2021-03-10 20:24:52 +1100

    Attempting to set up threading to capture the missing Lou codes

--- sboard_exec_thread.py
+++ sboard_exec_thread.py
@@ -14,6 +14,7 @@
 import os
 import json
 import datetime
+import threading
 from time import time, ctime, sleep
 import pigpio as GPIO
 import serial
@@ -84,6 +85,36 @@ comms_end_char = bytes.fromhex(comms['end_char'])
 max_comm_string = comms['max_comm_string']
 
 ser.reset_input_buffer()  # flush out any rubbish already there
+
+serial_flag = 0
+
+def thread_serial(ser):
+    global serial_flag, x
+    if ser.in_waiting > 0:
+        Comms_Error = ''
+    # print('*')
+        if ser.read() != comms_start_char:
+            Comms_Error = 'Message Discarded!'
+            st1 = ser.read_until(comms_end_char, max_comm_string)
+            log_it(logging, f'Comms: {st1}')
+            log_it(logging, Comms_Error)
+            serial_flag = 0
+        else:
+            st1 = ser.read_until(comms_end_char, max_comm_string).decode("utf-8", "replace")
+            x = st1.replace(chr(65533), ' ')
+            x = x[:len(x) - 1]  # remove the Ctrl t char at the end
+            print(f'Current Comms: {x}')
+            if x[0] != 'K' or 'L':
+                serial_flag = check_allint(x)  # check what has been received is all integers
+            else:
+                serial_flag = True  # mix of char and numbers so can't check
+            if serial_flag == True:
+                Last_Comms = f'Message Received: {x}'
+                Comms_Error = f'Valid Message! Length: {len(x)}'
+            else:
+                Comms_Error = f'Message Corrupt: {x} Length: {len(x)}'
+                log_it(logging, f'Comms: {x}')
+                log_it(logging, Comms_Error)
 
 
 # set up the port arrays. These show the position of the digit no. in the scoreboard dictionary
@@ -358,34 +389,10 @@ if bool(q_loc):
 
 Message2 = 'Scoreboard initialised - waiting for commands on serial port!'
 send_screen()
+ser_thread = threading.Thread(target=thread_serial, args=(ser,), daemon=True)
+ser_thread.start()
 
 while True:
-    serial_flag = 0
-    if ser.in_waiting > 0:
-        Comms_Error = ''
-        #print('*')
-        if ser.read() != comms_start_char:
-            Comms_Error = 'Message Discarded!'
-            st1 = ser.read_until(comms_end_char, max_comm_string)
-            log_it(logging, f'Comms: {st1}')
-            log_it(logging, Comms_Error)
-            serial_flag = 0
-        else:
-            st1 = ser.read_until(comms_end_char, max_comm_string).decode("utf-8", "replace")
-            x = st1.replace(chr(65533), ' ')
-            x =  x[:len(x)-1]    #remove the Ctrl t char at the end
-            print(f'Current Comms: {x}')
-            if x[0] != 'K' or 'L':
-                serial_flag = check_allint(x)  # check what has been received is all integers
-            else:
-                serial_flag = True  # mix of char and numbers so can't check
-            if serial_flag == True:
-                Last_Comms = f'Message Received: {x}'
-                Comms_Error = f'Valid Message! Length: {len(x)}'
-            else:
-                Comms_Error = f'Message Corrupt: {x} Length: {len(x)}'
-            log_it(logging, f'Comms: {x}')
-            log_it(logging, Comms_Error)
 
     if serial_flag == 1:
 
